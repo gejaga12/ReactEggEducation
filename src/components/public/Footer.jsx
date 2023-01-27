0430939f3da2be52dc2b892b52f868b37a5e0d77
gejaga12 <gejaga12@hotmail.com>
2023-01-27 16:32:43 -0300

    class Main creada

--- src/components/public/Footer.jsx
+++ src/components/public/Footer.jsx
@@ -2,35 +2,35 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div class="container">
-      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <p class="col-md-4 mb-0 text-muted">
+    <div className="container">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p className="col-md-4 mb-0 text-muted">
           &copy; 2022 Gerardo García Frontend Developer. Inc.
         </p>
 
-        <ul class="nav col-md-4 justify-content-end">
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-muted">
+        <ul className="nav col-md-4 justify-content-end">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
               Home
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-muted">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
               Features
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-muted">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
               Pricing
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-muted">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
               FAQs
             </a>
           </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link px-2 text-muted">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-muted">
               About
             </a>
           </li>
